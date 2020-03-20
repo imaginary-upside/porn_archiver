042b943d6702b767f3f7b08396771a566a017315
imaginary-upside <imaginary-upside@protonmail.ch>
2020-03-20 17:55:25 -0400

    set downloaded images last_modified date

--- src/extractor/base.cr
+++ src/extractor/base.cr
@@ -1,4 +1,6 @@
 require "uri"
+require "file_utils"
+require "time"
 
 module PornArchiver::Extractor
   abstract class Base
@@ -24,7 +26,13 @@ module PornArchiver::Extractor
 
       HTTP::Client.get(image) do |r|
         return unless r.success?
+
         File.write(dest, r.body_io)
+
+        if lm = r.headers["last-modified"]?
+          t = Time.parse(lm, "%a, %d %b %Y %T", Time::Location::UTC)
+          FileUtils.touch(dest, t)
+        end
       end
     end
   end
