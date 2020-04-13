703ebdcd82c4794c9cca612e78faa6d6cebe73cc
imaginary-upside <imaginary-upside@protonmail.ch>
2020-04-13 11:28:23 -0400

    Add basic arguments support

--- src/porn_archiver.cr
+++ src/porn_archiver.cr
@@ -1,9 +1,23 @@
 require "./extractor/factory"
 require "uri"
+require "option_parser"
 
 module PornArchiver
   VERSION = "0.1.0"
 
-  r = Extractor.factory ARGV[0]
-  r.start if r
+  base = "archive"
+
+  OptionParser.parse do |parser|
+    parser.banner = "Usage: porn_archiver [arguments] url1 url2 ..."
+    
+    parser.on("-o OUT", "--out=OUT", "Base directory to store archives") do |v|
+      base = v
+    end
+    parser.on("-h", "--help", "Show this help") { puts parser }
+  end
+
+  ARGV.each do |url|
+    r = Extractor.factory url
+    r.start if r
+  end
 end
